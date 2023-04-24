fd36bd9f8f8e6a21e2932e1c16bc4218ced20eae
Antonin.duvauchel.etu <antonin.duvauchel.etu@univ-lille.fr>
2023-04-24 21:04:50 +0300

    arborescence et fivhier de configuration

---
+++ src/view/View.js
@@ -0,0 +1,12 @@
+export default class View {
+  constructor(element) {
+    this.element = element;
+    this.hide();
+  }
+  show() {
+    this.element.style.display = "block";
+  }
+  hide() {
+    this.element.style.display = "none";
+  }
+}
