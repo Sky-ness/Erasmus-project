3eeb8ba9a4eeb6872e80e58e01396fa1b5bd7e98
Antonin.duvauchel.etu <antonin.duvauchel.etu@univ-lille.fr>
2023-04-28 15:09:19 +0300

    all the data is here

--- client/src/main.js
+++ client/src/main.js
@@ -15,7 +15,6 @@ const linkApi = 'api/oliveTrees';
 // const addView = new AddView(".olive-trees .editView");
 
 const mainView = document.querySelector('.olive-trees .mainView');
-
 const addView = document.querySelector('.olive-trees .addView');
 
 addView.style.display = 'none';
@@ -25,27 +24,75 @@ fetch(linkApi)
 	.then(response => response.json())
 	.then(data => {
 		oliveTrees = data;
-		displayTrees(oliveTrees);
+		displayLessDataTrees(oliveTrees);
 	});
 
-function displayTrees(data) {
+function displayAllDataTrees(data) {
 	data.forEach(oliveTree => {
-		mainView.innerHTML +=
-			`<p>${oliveTree.column1}` +
-			`${oliveTree.treeCode}` +
-			`${oliveTree.longitude}` +
-			`${oliveTree.latitude}` +
-			`${oliveTree.nisi}` +
-			`${oliveTree.perimAt1m30}` +
-			`${oliveTree.basePerimeter}` +
-			`${oliveTree.height}` +
-			`${oliveTree.branch}` +
-			`${oliveTree.numberOfBranches}` +
-			`${oliveTree.cavitation}` +
-			`${oliveTree.trunkShapes}` +
-			`${oliveTree.trunkTorsion}` +
-			`${oliveTree.landUse}` +
-			`${oliveTree.paratiriseis}</p>`;
+		mainView.innerHTML += `<table>
+			<tr>
+				<th>Primary key</th>
+				<th>Coordonate GPS</th>
+				<th>Perimeter</th>
+				<th>Branches</th>
+				<th>Height</th>
+			</tr>
+			<tr>
+				<td>${oliveTree.column1}</td>
+				<td>X: ${oliveTree.longitude} | Y: ${oliveTree.latitude}</td>
+				<td>base: ${oliveTree.basePerimeter} | at-1m30:${oliveTree.perimAt1m30}</td>
+				<td>number: ${oliveTree.numberOfBranches} | size: ${oliveTree.branch}</td>
+				<td>${oliveTree.height}</td>
+			</tr>
+			<tr>
+				<th>Tree Code</th>
+				<th>Cavitation</th>
+				<th>Trunk shapes</th>
+				<th>Trunk torsion</th>
+			</tr>
+			<tr>
+				<td>${oliveTree.treeCode}</td>
+				<td>${oliveTree.cavitation}</td> 
+				<td>${oliveTree.trunkShapes}</td> 
+				<td>${oliveTree.trunkTorsion}</td>
+			</tr> 
+			<tr>
+				<th>Nisi</th>
+				<th>Land use</th>
+				<th>Paratiriseis</th>
+			</tr>
+			<tr>
+				<td>${oliveTree.nisi}</td> 
+				<td>${oliveTree.landUse}</td> 
+				<td>${oliveTree.paratiriseis}</td>
+			</tr>
+			<tr>
+				<button>less information</button>
+			</tr>
+		</table>`;
+	});
+}
+function displayLessDataTrees(data) {
+	data.forEach(oliveTree => {
+		mainView.innerHTML += `<table>
+				<tr>
+					<th>Primary key</th>
+					<th>Coordonate GPS</th>
+					<th>Perimeter</th>
+					<th>Number Of Branches</th>
+					<th>Height</th>
+				</tr>
+				<tr>
+					<td>${oliveTree.column1}</td>
+					<td>X: ${oliveTree.longitude} | Y: ${oliveTree.latitude}</td>
+					<td>base: ${oliveTree.basePerimeter} | at-1m30: ${oliveTree.perimAt1m30}</td>
+					<td>number: ${oliveTree.numberOfBranches} | size: ${oliveTree.branch}</td>
+					<td>${oliveTree.height}</td>
+				</tr>
+				<tr>
+					<td colspan=\"5\"><a href=\"#\">more information</a></td>
+				</tr>
+			</table>`;
 	});
 }
 
