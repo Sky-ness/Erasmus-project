3ea9e7ed2c417db6b059e260f75e83e488bb88a7
Antonin.duvauchel.etu <antonin.duvauchel.etu@univ-lille.fr>
2023-05-10 13:25:49 +0300

    ajout du data slicer + ajout des boutton fonctionnel

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,5 @@
 //import EditView from "./view/EditView.js";
 //import AddView from "./view/addView.js";
-import { doc } from 'prettier';
 import renderTreeThumbnail from './RenderTreeThumbnail.js';
 
 console.log('hello world');
@@ -14,23 +13,51 @@ const linkApi = 'api/oliveTrees';
 // const addView = new AddView(".olive-trees .editView");
 
 const mainView = document.querySelector('.olive-trees .mainView');
-const addView = document.querySelector('.olive-trees .addView');
 const searchForm = document.querySelector('.searchForm');
-const addViewForm = document.querySelector('.addViewForm');
 const modal = document.querySelector('.modal');
 const toggleAddButton = document.querySelector('.toggleAddButton');
 const close = document.querySelector('.close');
+const tabButton1 = document.querySelector('.tabButton');
+// const tabButton = document.querySelectorAll('.tabButton button');
 
 let oliveTrees = [];
-let oliveTreesLimited = [];
+
+searchForm.addEventListener('submit', handleSearchFormSubmit);
+
+function sliceData(data, index) {
+	if (index == 0) return data.slice(index, 5);
+	else return data.slice(5 * index, 5 + 5 * index);
+}
+
+function renderTreeList(search = '') {
+	let html = '';
+	oliveTrees
+		.filter(tree => tree.column1.toLowerCase().includes(search.toLowerCase()))
+		.forEach(tree => (html += renderTreeThumbnail(tree)));
+
+	document.querySelector('.olive-trees .mainView').innerHTML = html;
+}
+
+function handleSearchFormSubmit(event) {
+	event.preventDefault();
+	const searchInput = searchForm.querySelector('[name=search]');
+	renderTreeList(searchInput.value);
+}
 
 fetch('api/oliveTrees')
 	.then(response => response.json())
 	.then(data => {
 		oliveTrees = data;
-		oliveTreesLimited = data.slice(0, 5);
 		// renderTreeList();
-		displayLessDataTrees(oliveTreesLimited);
+		displayLessDataTrees(sliceData(oliveTrees, 0));
+		generateButton(oliveTrees);
+		const tabButton = document.querySelectorAll('.tabButton button');
+		tabButton.forEach((indexBtn, index) => {
+			indexBtn.addEventListener('click', () => {
+				console.log(index);
+				displayLessDataTrees(sliceData(oliveTrees, index));
+			});
+		});
 	});
 
 function displayAllDataTrees(data) {
@@ -78,7 +105,58 @@ function displayAllDataTrees(data) {
 		</table>`;
 	});
 }
+function displayAllDataTree(oliveTree) {
+	mainView.innerHTML += `<table>
+			<tr>
+				<th>Primary key</th>
+				<th>Coordonate GPS</th>
+				<th>Perimeter</th>
+				<th>Branches</th>
+				<th>Height</th>
+			</tr>
+			<tr>
+				<td>${oliveTree.column1}</td>
+				<td>X: ${oliveTree.longitude} | Y: ${oliveTree.latitude}</td>
+				<td>base: ${oliveTree.basePerimeter} | at-1m30:${oliveTree.perimAt1m30}</td>
+				<td>number: ${oliveTree.numberOfBranches} | size: ${oliveTree.branch}</td>
+				<td>${oliveTree.height}</td>
+			</tr>
+			<tr>
+				<th>Tree Code</th>
+				<th>Cavitation</th>
+				<th>Trunk shapes</th>
+				<th>Trunk torsion</th>
+			</tr>
+			<tr>
+				<td>${oliveTree.treeCode}</td>
+				<td>${oliveTree.cavitation}</td> 
+				<td>${oliveTree.trunkShapes}</td> 
+				<td>${oliveTree.trunkTorsion}</td>
+			</tr> 
+			<tr>
+				<th>Nisi</th>
+				<th>Land use</th>
+				<th>Paratiriseis</th>
+			</tr>
+			<tr>
+				<td>${oliveTree.nisi}</td> 
+				<td>${oliveTree.landUse}</td> 
+				<td>${oliveTree.paratiriseis}</td>
+			</tr>
+			<tr class=\"less\">
+				<button>less information</button>
+			</tr>
+		</table>`;
+}
+function generateButton(data) {
+	let j = 0;
+	for (let i = 0; i <= data.length; i += 5) {
+		tabButton1.innerHTML += `<button>${j}</button>`;
+		j++;
+	}
+}
 function displayLessDataTrees(data) {
+	mainView.innerHTML = '';
 	data.forEach(oliveTree => {
 		mainView.innerHTML += `<table>
 				<tr>
@@ -96,29 +174,12 @@ function displayLessDataTrees(data) {
 					<td>number: ${oliveTree.numberOfBranches} | size: ${oliveTree.branch}</td>
 					<td>${oliveTree.height}</td>
 				</tr>
-				<tr>
-					<td colspan=\"5\"><a href=\"#\">more information</a></td>
+				<tr class=\"more\">
+					<td colspan=\"5\"><a>more information</a></td>
 				</tr>
 			</table>`;
 	});
 }
-
-function renderTreeList(search = '') {
-	let html = '';
-	oliveTrees
-		.filter(tree => tree.column1.toLowerCase().includes(search.toLowerCase()))
-		.forEach(tree => (html += renderTreeThumbnail(tree)));
-
-	document.querySelector('.olive-trees .mainView').innerHTML = html;
-}
-
-function handleSearchFormSubmit(event) {
-	event.preventDefault();
-	const searchInput = searchForm.querySelector('[name=search]');
-	renderTreeList(searchInput.value);
-}
-
-searchForm.addEventListener('submit', handleSearchFormSubmit);
 
 //ouvre le modal
 toggleAddButton.onclick = function () {
