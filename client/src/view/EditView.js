c5dc273b4533398eaff1eb02b0f92e233e6e7fb8
Audrey <creditential.helper>
2023-06-12 17:23:20 +0200

    passage de index.html à update.html

--- client/src/view/EditView.js
+++ client/src/view/EditView.js
@@ -1,10 +1,9 @@
 import View from './View.js';
 
 export default class EditView extends View {
-	oliveTree;
-	constructor(element, oliveTree) {
+	constructor(element) {
 		super(element);
-		this.oliveTree = oliveTree;
+		this.hide();
 	}
 
 	edit(primaryKey, oliveTree) {
@@ -20,4 +19,49 @@ export default class EditView extends View {
 			.catch(error => console.error(error));
 	}
 }
-// function PopupView(oliveTree) {}
+
+// 		this.close = this.element.querySelector('.close');
+// 		this.close.addEventListener('click', () => this.hide());
+
+// 		// window.addEventListener('click', event => {
+// 		// 	event.preventDefault();
+// 		// 	if (event.target == this.element) {
+// 		// 		this.hide();
+// 		// 	}
+// 		// });
+
+// 		// Écouter l'événement de soumission du formulaire
+// 		form.addEventListener('submit', event => {
+// 			event.preventDefault();
+// 			console.log('post');
+
+// 			// Récupérer les valeurs du formulaire
+// 			const inputs = document.querySelectorAll('.addTreeForm input');
+
+// 			const formData = {};
+
+// 			inputs.forEach(field => {
+// 				const fieldName = field.getAttribute('name');
+// 				let fieldValue = field.value;
+// 				if (fieldValue === '') {
+// 					fieldValue = null;
+// 				}
+// 				formData[fieldName] = fieldValue;
+// 			});
+
+// 			// Créer un objet avec les données du formulaire
+
+// 			try {
+// 				fetch('http://localhost:8000/api/oliveTrees', {
+// 					method: 'POST',
+// 					headers: {
+// 						'Content-Type': 'application/json',
+// 					},
+// 					body: JSON.stringify(formData),
+// 				});
+// 			} catch (error) {
+// 				console.error(error);
+// 			}
+// 		});
+// 	}
+// }
