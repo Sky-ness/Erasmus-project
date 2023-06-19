e27de217dec816507b70f218550a79f7a69e1d0a
Audrey <creditential.helper>
2023-06-19 11:15:56 +0200

    <br> au lieu de | dans render

--- client/src/function/RenderTreeThumbnail.js
+++ client/src/function/RenderTreeThumbnail.js
@@ -16,9 +16,9 @@ export default function renderTreeThumbnail(tree) {
 			<div class="cell"> <a href=./update.html?id=${tree.id}><img src="images/website/pen2.ico" alt=edit/></a> </div>
 			
 			<div class="cell"> ${tree.id} </div>
-			<div class="cell"> X: ${tree.longitude} | Y: ${tree.latitude} </div>
-			<div class="cell"> base: ${tree.base_perimeter} | at-1m30: ${tree.perim_at_1m30} </div>
-			<div class="cell"> number: ${tree.number_of_branches} | size: ${tree.branch} </div>
+			<div class="cell"> X: ${tree.longitude} <br> Y: ${tree.latitude} </div>
+			<div class="cell"> base: ${tree.base_perimeter}  at-1m30: ${tree.perim_at_1m30} </div>
+			<div class="cell"> number: ${tree.number_of_branches} <br> size: ${tree.branch} </div>
 			<div class="cell"> ${tree.height} </div>
 			<div class="cell"> ${score} </div>
 			<div class="cell">  <a href=./delete.html?id=${tree.id}><img src="images/website/delete.ico" alt=delete/></a></div>
